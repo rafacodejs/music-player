5dfa14ec2aafda2a624966435721dff7f52313b3
Rafael Malpica <rafacodejs@gmail.com>
2022-10-18 15:41:49 -0400

    Modify: SongCard component, Discover component, MusicPlayer folder andfetch to the api

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set(
+        'X-RapidAPI-Key',
+        '560e5d8d4amsh29829d906abdf6fp164968jsnf8ea5f931ccc'
+      );
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
