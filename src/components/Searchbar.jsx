b6f133dbc90446cec3cc106925b609b600e5793b
Rafael Malpica <rafacodejs@gmail.com>
2022-10-27 07:46:41 -0400

    Test app

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,38 +1,36 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
 import { FiSearch } from 'react-icons/fi';
 
 const Searchbar = () => {
   const navigate = useNavigate();
   const [searchTerm, setSearchTerm] = useState('');
-
   const handleSubmit = (e) => {
     e.preventDefault();
-
     navigate(`/search/${searchTerm}`);
   };
-
   return (
     <form
       onSubmit={handleSubmit}
       autoComplete='off'
-      className='p-2 text-gray-400 focus-within:text-gray-600'
+      className='p-2 text-gray-200 focus-within:text-gray-600'
     >
       <label htmlFor='search-field' className='sr-only'>
-        Search all files
+        Search all songs
       </label>
       <div className='flex flex-row justify-start items-center'>
-        <FiSearch aria-hidden='true' className='w-5 h-5 ml-4' />
+        <div className='flex justify-center items-center w-[38px] h-[56px] ml-2 absolute bg-gray-300 rounded-md '>
+          <FiSearch className='text-[30px] text-black rounded-md ' />
+        </div>
         <input
           name='search-field'
           autoComplete='off'
           id='search-field'
-          className='flex-1 bg-transparent border-none placeholder-gray-500 outline-none text-base text-white p-4 font-poppins'
-          placeholder='Search'
+          placeholder='What do you want to listen to?'
           type='search'
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
+          className='flex-1 bg-gray-300 border-none outline-none placeholder-gray-500 text-base text-black rounded-md p-4 font-poppins ml-9'
         />
       </div>
     </form>
